73dd8430bc6410b41ffd883a8a9efd27c3661328
A-26 <A-26@A-26>
2024-04-29 10:03:01 +0700

    pertama

---
+++ latihan6/src/main/java/com/mycompany/latihan6/KelasKe6.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan6;
+
+/**
+ *
+ * @author A-26
+ */
+public class KelasKe6 {
+    public static void main(String[] args) {
+        System.out.println("aku mega");
+    }
+}
